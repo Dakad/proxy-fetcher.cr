e260dadd2a71fd691b32e401b5f3a1c6d7b94eec
Nipinium <nipinium@gmail.com>
2020-03-08 20:11:27 +0700

    bump version

--- src/proxy_fetcher.cr
+++ src/proxy_fetcher.cr
@@ -20,7 +20,7 @@ require "./proxy_fetcher/providers/http_tunnel"
 require "./proxy_fetcher/providers/xroxy"
 
 module ProxyFetcher
-  VERSION = "0.1.0"
+  VERSION = "0.2.0"
 
   def self.config
     @@config ||= ProxyFetcher::Configuration.new
